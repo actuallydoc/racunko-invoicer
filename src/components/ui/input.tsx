13097a2179c4a1ad57aab83cff1dd3d6a806f84f
Maj Knez <actuallydoc@gmail.com>
2023-06-18 11:42:37 +0200

    Empty interface shadcn component eslint

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,8 +2,10 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
