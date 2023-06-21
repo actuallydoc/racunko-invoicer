a7efb63eebb63d818a42909dfb6b71d13b024800
Maj Knez <actuallydoc@gmail.com>
2023-06-21 11:50:53 +0200

    home page image bug

--- src/components/Cards/DashboardCard.tsx
+++ src/components/Cards/DashboardCard.tsx
@@ -8,8 +8,10 @@ export default function DashboardCard() {
         <div>
             <Card>
                 <CardContent>
-                    {theme.theme === 'dark' ? <Image src='https://i.imgur.com/60JQ95p.png' width={500} height={500} alt='dashboard' /> : (
-                        <Image src='https://i.imgur.com/2WCBFP7.png' width={500} height={500} alt='dashboard' />)}
+
+
+                    {theme && theme.theme === 'dark' && <Image src='https://i.imgur.com/60JQ95p.png' width={500} height={500} alt='dashboard' />}
+                    {theme && theme.theme === 'light' && <Image src='https://i.imgur.com/2WCBFP7.png' width={500} height={500} alt='dashboard' />}
                 </CardContent>
             </Card>
 
